8964bb90ffbfdc5fe405c822b3abdc4788c56ec2
isharadilshanra <isharadilshanra11@gmail.com>
2024-12-25 13:31:46 +0530

    handle conditions

--- vite-project/latest_project/vite-project/src/components/ListGroup.tsx
+++ vite-project/latest_project/vite-project/src/components/ListGroup.tsx
@@ -1,6 +1,28 @@
+// import { Fragment } from "react";
+import Message from "../Message";
 
 function ListGroup() {
-    return <div><h1>This is the ListGroup</h1></div>
+  let items = ["option 1", "option 2", "option 3", "option 4", "option 5"];
+  //items = [];
+  //const messge = items.length === 0 ? <h3> No items found</h3> : null;
 
+  const getMessge = () => {
+    return items.length === 0 ? <h3> No items found</h3> : null;
+  };
+
+  return (
+    <>
+      <h1>List Group</h1>
+      {getMessge()}
+      {items.length === 0 && <h3>The list is empty</h3>}
+      <ul className="list-group">
+        {items.map((item, index) => (
+          <li key={index} className="list-group-item">
+            {index + 1}. {item}
+          </li>
+        ))}
+      </ul>
+    </>
+  );
 }
 export default ListGroup;
